88d38e44304f117c40ec925be4c25c6552b1be55
AnarbaevErbol <anarbaeverik96@gmail.com>
2022-05-04 10:36:13 +0300

    init commit

---
+++ src/main/java/peassoft_java-5/App.java
@@ -0,0 +1,13 @@
+package peassoft_java-5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
